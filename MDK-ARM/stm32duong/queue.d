f670e7254a7d217fcce6690628a1652ec550171e
Nguyen <nnnguyen1604@gmail.com>
2024-06-23 01:40:06 +0700

    Initial commit

---
+++ MDK-ARM/stm32duong/queue.d
@@ -0,0 +1,15 @@
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/queue.c
+stm32duong\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+stm32duong\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+stm32duong\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+stm32duong\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stm32duong\queue.o: ../Core/Inc/FreeRTOSConfig.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+stm32duong\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h
